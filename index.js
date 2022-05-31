a127cf26b7c3523d3c1ef02e6298c1f42490b655
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-05-31 14:48:24 +0200

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-const port = 3000;
+
+const port = process.env.PORT;
 const app = express();
+app.use(cors());
 
-app.use("/questions",(req,res)=> {
-    return res.json({ msg: "Hello"});
-})
-
-
-app.listen(port, ()=> {
-    console.log(`quiz ${port}`);
+const questionSchema=new mongoose.Schema({
+    question: String,
+    answer: Array,
+    correct: String,
 });
+const Question = mongoose.model("Question", questionSchema, "questions");
+
+app.use("/questions", async (req, res) => {
+    try{
+      const questions=await Question.find().exec();
+      res.json(questions);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+    //return res.json({ msg: "Hello"});
+});
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+    app.listen(port, () => {
+      console.log(`Quiz ${port}`);
+   });
+  });
+
+
